963390a7d291a2d7e82bd76e3edbf5695291adac
Walfwnstaine <121248230+Walfenstaine@users.noreply.github.com>
2025-02-06 08:24:32 +0300

    Delete Assets directory

--- Assets/Scripts/Data/Inventared.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections.Generic;
-
-[CreateAssetMenu(fileName = "Inventared", menuName = "ScriptableMemory/Inventared", order = 1)]
-public class Inventared : ScriptableObject
-{
-    public Sprite[] icon;
-}
